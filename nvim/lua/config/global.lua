b0459e29aa5eedee1eced6d02baf9c71fd5ba8b7
Dan Michell <dan.michell1000@gmail.com>
2025-09-02 17:18:21 +0100

    remove nvim config

--- nvim/lua/config/global.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- disable netrw for nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- nvim/lua/custom/init.lua
+++
@@ -1,1 +0,0 @@
-require("custom.floating_terminal")
